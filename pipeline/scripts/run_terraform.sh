2559f493b840b99deb86cf69b8a0b20a58cc962c
Davixo23 <david.utic@gmail.com>
2025-07-02 22:30:56 -0400

    modificaciones 6

--- pipeline/scripts/run_terraform.sh
+++ pipeline/scripts/run_terraform.sh
@@ -12,8 +12,11 @@ terraform plan -out=tfplan
 if [ "$ACTION" = "apply" ]; then
   terraform apply -auto-approve tfplan
 
-  # Guardar la IP pública en un archivo
+  # Guardar la IP pública en un archivo dentro de terraform
   terraform output -raw instance_public_ip > instance_public_ip.txt
+
+  # Copiar el archivo a la carpeta pipeline para que Jenkins lo encuentre
+  cp instance_public_ip.txt ../../pipeline/
 
 elif [ "$ACTION" = "destroy" ]; then
   terraform destroy -auto-approve
